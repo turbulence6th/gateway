c51c13022edb8c110c90ac94b6c2b66493168cf4
Oguz Tanrikulu <turbulence6th@gmail.com>
2020-09-27 20:51:05 +0300

    fix url

--- docker-build.sh
+++
@@ -1,4 +0,0 @@
-mvn clean install
-docker rmi -f turbulence6th/gateway:latest
-docker build -t turbulence6th/gateway .
-docker run -p 80:8080 -t turbulence6th/gateway --env-file /root/envs/gateway.env
