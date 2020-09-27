5994e4ba554b3f1f41626941e3b52f4193cab051
Oguz Tanrikulu <turbulence6th@gmail.com>
2020-09-27 18:10:50 +0300

    set env file

--- docker-build.sh
+++ docker-build.sh
@@ -1,4 +1,4 @@
 mvn clean install
 docker rmi turbulence6th/gateway:latest
 docker build -t turbulence6th/gateway .
-docker run -p 80:8080 -t turbulence6th/gateway
+docker run -p 80:8080 -t turbulence6th/gateway --env-file ../gateway.env
