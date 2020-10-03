836b7f9bae4576ae4412e4b6bd00a24c7c2d9bd3
Oguz Tanrikulu <turbulence6th@gmail.com>
2020-10-03 10:53:56 +0300

    refactor import

--- src/main/java/com/turbulence6th/gateway/config/GatewayDiscoveryConfiguration.java
+++ src/main/java/com/turbulence6th/gateway/config/GatewayDiscoveryConfiguration.java
@@ -2,14 +2,10 @@ package com.turbulence6th.gateway.config;
 
 import com.turbulence6th.gateway.dao.OrientationDao;
 import com.turbulence6th.gateway.entity.Orientation;
-import org.apache.catalina.Context;
 import org.apache.catalina.connector.Connector;
-import org.apache.tomcat.util.descriptor.web.SecurityCollection;
-import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
