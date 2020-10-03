331973acd22a282f88b9452a3407267ea62cbc5b
Oguz Tanrikulu <turbulence6th@gmail.com>
2020-10-03 10:57:44 +0300

    revert

--- src/main/java/com/turbulence6th/gateway/config/GatewayDiscoveryConfiguration.java
+++ src/main/java/com/turbulence6th/gateway/config/GatewayDiscoveryConfiguration.java
@@ -2,10 +2,6 @@ package com.turbulence6th.gateway.config;
 
 import com.turbulence6th.gateway.dao.OrientationDao;
 import com.turbulence6th.gateway.entity.Orientation;
-import org.apache.catalina.connector.Connector;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -15,10 +11,6 @@ import java.util.List;
 
 @Configuration
 public class GatewayDiscoveryConfiguration {
-
-    @Value( "${turbulence6th.http.port}" )
-    private Integer httpPort;
-
 
     @Bean
     public RouteLocator routes(RouteLocatorBuilder builder, OrientationDao orientationDao) {
@@ -33,14 +25,5 @@ public class GatewayDiscoveryConfiguration {
         }
 
         return  b.build();
-    }
-
-    @Bean
-    public ConfigurableServletWebServerFactory webServerFactory() {
-        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
-        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
-        connector.setPort(httpPort);
-        factory.addAdditionalTomcatConnectors(connector);
-        return factory;
     }
 }
