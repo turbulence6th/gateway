8dfd6ab66ebf00424d01d682c5586febc1b54794
Oguz Tanrikulu <turbulence6th@gmail.com>
2020-10-04 13:31:08 +0300

    fix forward

--- src/main/java/com/turbulence6th/gateway/config/SecurityConfiguration.java
+++ src/main/java/com/turbulence6th/gateway/config/SecurityConfiguration.java
@@ -1,10 +1,17 @@
 package com.turbulence6th.gateway.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.netty.NettyReactiveWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
+
+import javax.annotation.PostConstruct;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @Configuration
 public class SecurityConfiguration {
@@ -12,12 +19,20 @@ public class SecurityConfiguration {
     @Value("${server.ssl.enabled:false}")
     private boolean sslEnabled;
 
-    @Bean
-    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
-        if (sslEnabled) {
-            http.redirectToHttps();
-        }
-
-        return http.build();
+    @PostConstruct
+    public void startRedirectServer() {
+        NettyReactiveWebServerFactory httpNettyReactiveWebServerFactory = new NettyReactiveWebServerFactory(8080);
+        httpNettyReactiveWebServerFactory.getWebServer((request, response) -> {
+            URI uri = request.getURI();
+            URI httpsUri;
+            try {
+                httpsUri = new URI("https", uri.getUserInfo(), uri.getHost(), 8443, uri.getPath(), uri.getQuery(), uri.getFragment());
+            } catch (URISyntaxException e) {
+                return Mono.error(e);
+            }
+            response.setStatusCode(HttpStatus.MOVED_PERMANENTLY);
+            response.getHeaders().setLocation(httpsUri);
+            return response.setComplete();
+        }).start();
     }
 }
