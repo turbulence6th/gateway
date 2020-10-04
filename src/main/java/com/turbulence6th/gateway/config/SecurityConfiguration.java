7d78b8ab7dc8253d9611939be39047b6ddcc2d9a
Oguz Tanrikulu <turbulence6th@gmail.com>
2020-10-04 13:37:43 +0300

    fix forward

--- src/main/java/com/turbulence6th/gateway/config/SecurityConfiguration.java
+++ src/main/java/com/turbulence6th/gateway/config/SecurityConfiguration.java
@@ -2,11 +2,8 @@ package com.turbulence6th.gateway.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.embedded.netty.NettyReactiveWebServerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.server.SecurityWebFilterChain;
 import reactor.core.publisher.Mono;
 
 import javax.annotation.PostConstruct;
@@ -21,18 +18,20 @@ public class SecurityConfiguration {
 
     @PostConstruct
     public void startRedirectServer() {
-        NettyReactiveWebServerFactory httpNettyReactiveWebServerFactory = new NettyReactiveWebServerFactory(8080);
-        httpNettyReactiveWebServerFactory.getWebServer((request, response) -> {
-            URI uri = request.getURI();
-            URI httpsUri;
-            try {
-                httpsUri = new URI("https", uri.getUserInfo(), uri.getHost(), 8443, uri.getPath(), uri.getQuery(), uri.getFragment());
-            } catch (URISyntaxException e) {
-                return Mono.error(e);
-            }
-            response.setStatusCode(HttpStatus.MOVED_PERMANENTLY);
-            response.getHeaders().setLocation(httpsUri);
-            return response.setComplete();
-        }).start();
+        if (sslEnabled) {
+            NettyReactiveWebServerFactory httpNettyReactiveWebServerFactory = new NettyReactiveWebServerFactory(8080);
+            httpNettyReactiveWebServerFactory.getWebServer((request, response) -> {
+                URI uri = request.getURI();
+                URI httpsUri;
+                try {
+                    httpsUri = new URI("https", uri.getUserInfo(), uri.getHost(), 443, uri.getPath(), uri.getQuery(), uri.getFragment());
+                } catch (URISyntaxException e) {
+                    return Mono.error(e);
+                }
+                response.setStatusCode(HttpStatus.MOVED_PERMANENTLY);
+                response.getHeaders().setLocation(httpsUri);
+                return response.setComplete();
+            }).start();
+        }
     }
 }
